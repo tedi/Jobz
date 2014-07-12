0a1e70f8e2ca2f58f1fb6a940121a461a903534c
Tedi Konda <tedikonda@gmail.com>
2014-07-12 16:34:45 -0400

    Schema merge.

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20140712201011) do
+
+  create_table "job_histories", force: true do |t|
+    t.integer  "seeker_id"
+    t.integer  "year_start"
+    t.integer  "year_end"
+    t.string   "title"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "job_seekers", force: true do |t|
     t.string   "objective"
@@ -24,43 +33,6 @@ ActiveRecord::Schema.define(version: 20140712201011) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "image"
-  end
-
-  create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "first_name"
-    t.string   "last_name"
-    t.integer  "role_id"
-    t.integer  "recruiter_id"
-    t.integer  "job_seeker_id"
-    t.string   "provider"
-    t.string   "uid"
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
-ActiveRecord::Schema.define(version: 20140712173331) do
-
-  create_table "job_histories", force: true do |t|
-    t.integer  "seeker_id"
-    t.integer  "year_start"
-    t.integer  "year_end"
-    t.string   "title"
-    t.string   "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "jobs", force: true do |t|
@@ -85,5 +57,30 @@ ActiveRecord::Schema.define(version: 20140712173331) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "role_id"
+    t.integer  "recruiter_id"
+    t.integer  "job_seeker_id"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
